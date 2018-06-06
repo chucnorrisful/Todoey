bdb0b07a62fd161eb9deaf4518f3dd61549ef11d
Benjamin Möckl <benjamin.moeckl@venture-ship.com>
2018-06-06 15:16:39 +0200

    To do list complete with no data persistence.

---
+++ Todoey/TodoListViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Benjamin Möckl on 07.04.18.
+//  Copyright © 2018 Benjamin Möckl. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+
+    let itemArray = ["Find Milk", "Buy Eggs", "Destroy Demogorgon"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    //MARK - Tableview Datasource Methods
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+        
+    }
+    
+    //MARK - TableView Delegate Methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //print(tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark)
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+             tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+             tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+       
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+}
+
